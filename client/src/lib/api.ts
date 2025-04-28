bb91ab9dd83c6389626e158ef43686c128ef9c38
Vinicius Paula <viniciuspaularibeiro02@gmail.com>
2025-04-28 11:26:50 -0300

    Arrumando erro da busca no deploy

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,11 +1,11 @@
 export async function fetchAlunos() {
-    const res = await fetch("http://localhost:5000/api/alunos");
+    const res = await fetch("/api/alunos");
     if (!res.ok) throw new Error("Erro ao buscar alunos");
     return res.json();
   }
 
 export async function cadastrarAluno({ name, class: turma, shift }: { name: string, class: string, shift: string }) {
-  const res = await fetch("http://localhost:5000/api/alunos", {
+  const res = await fetch("/api/alunos", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ name, class: turma, shift }),
@@ -15,7 +15,7 @@ export async function cadastrarAluno({ name, class: turma, shift }: { name: stri
 }
 
 export async function editarAluno(id: number, { name, class: turma, shift }: { name: string, class: string, shift: string }) {
-  const res = await fetch(`http://localhost:5000/api/alunos/${id}`, {
+  const res = await fetch(`/api/alunos/${id}`, {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ name, class: turma, shift }),
@@ -25,7 +25,7 @@ export async function editarAluno(id: number, { name, class: turma, shift }: { n
 }
 
 export async function deletarAluno(id: number) {
-  const res = await fetch(`http://localhost:5000/api/alunos/${id}`, {
+  const res = await fetch(`/api/alunos/${id}`, {
     method: "DELETE"
   });
   if (!res.ok) throw new Error("Erro ao deletar aluno");
@@ -33,7 +33,7 @@ export async function deletarAluno(id: number) {
 }
 
 export async function fetchNotasAluno(studentId: number) {
-  const res = await fetch(`http://localhost:5000/api/alunos/${studentId}/grades`);
+  const res = await fetch(`/api/alunos/${studentId}/grades`);
   if (!res.ok) throw new Error("Erro ao buscar notas do aluno");
   return res.json();
 }
