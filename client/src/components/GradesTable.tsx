368052581a9a13f6c894aae07a9a0c0d332a0da7
Vinicius Paula <viniciuspaularibeiro02@gmail.com>
2025-04-29 21:40:30 -0300

    Update GradesTable.tsx

--- client/src/components/GradesTable.tsx
+++ client/src/components/GradesTable.tsx
@@ -547,14 +547,14 @@ const GradesTable = forwardRef<any, GradesTableProps>(({ passingGrade }, ref) =>
       doc.setFillColor(100, 149, 237);
       doc.rect(margin + 80, legendY, 10, 5, 'F');
       doc.setFillColor(67, 160, 71);
-      doc.rect(margin + 150, legendY, 10, 5, 'F'); // Reduzido de 160 para 150
+      doc.rect(margin + 154, legendY, 10, 5, 'F'); // Aumentado de 150 para 154
       
       doc.setFont('helvetica', 'bold');
       doc.setFontSize(10);
       doc.setTextColor(80, 80, 80);
-      doc.text('Abaixo de 21', margin + 13, legendY + 4);
-      doc.text('Entre 21 e 26,9', margin + 93, legendY + 4);
-      doc.text('Acima de 26,9', margin + 163, legendY + 4); // Reduzido de 173 para 163
+      doc.text('Abaixo de 7', margin + 13, legendY + 4);
+      doc.text('Entre 7 e 8,9', margin + 93, legendY + 4);
+      doc.text('Acima de 8,9', margin + 167, legendY + 4); // Aumentado de 163 para 167
       doc.setFont('helvetica', 'normal');
       doc.setFontSize(8);
       
@@ -1292,14 +1292,14 @@ const GradesTable = forwardRef<any, GradesTableProps>(({ passingGrade }, ref) =>
       doc.setFillColor(100, 149, 237);
       doc.rect(margin + 80, legendY, 10, 5, 'F');
       doc.setFillColor(67, 160, 71);
-      doc.rect(margin + 150, legendY, 10, 5, 'F'); // Reduzido de 160 para 150
+      doc.rect(margin + 154, legendY, 10, 5, 'F'); // Aumentado de 150 para 154
       
       doc.setFont('helvetica', 'bold');
       doc.setFontSize(10);
       doc.setTextColor(80, 80, 80);
       doc.text('Abaixo de 7', margin + 13, legendY + 4);
       doc.text('Entre 7 e 8,9', margin + 93, legendY + 4);
-      doc.text('Acima de 8,9', margin + 163, legendY + 4); // Ajustado para acompanhar o retângulo verde
+      doc.text('Acima de 8,9', margin + 167, legendY + 4); // Aumentado de 163 para 167
       doc.setFont('helvetica', 'normal');
       doc.setFontSize(8);
       
